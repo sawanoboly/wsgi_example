1f3b493d8b80b867dd098ccba4ad2009c73ccad0
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-15 10:46:23 +0900

    sample Flask

--- wsgi.py
+++ wsgi.py
@@ -1,66 +1,9 @@
-import logging
-import logging.handlers
-import numpy
+from flask import Flask
+app = Flask(__name__)
 
-from wsgiref.simple_server import make_server
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-
-# Create logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-# Handler 
-LOG_FILE = './sample-app.log'
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
-handler.setLevel(logging.INFO)
-
-# Formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# Add Formatter to Handler
-handler.setFormatter(formatter)
-
-# add Handler to Logger
-logger.addHandler(handler)
-
-welcome = """
-<!DOCTYPE html>
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Welcome</title>
-</head>
-<body id="sample">
-Hello Python with numpy.
-</body>
-</html>
-"""
-
-def application(environ, start_response):
-    path    = environ['PATH_INFO']
-    method  = environ['REQUEST_METHOD']
-    if method == 'POST':
-        try:
-            if path == '/':
-                request_body_size = int(environ['CONTENT_LENGTH'])
-                request_body = environ['wsgi.input'].read(request_body_size).decode()
-                logger.info("Received message: %s" % request_body)
-            elif path == '/scheduled':
-                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
-        except (TypeError, ValueError):
-            logger.warning('Error retrieving request body for async work.')
-        response = ''
-    else:
-        response = welcome
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-
-    start_response(status, headers)
-    return [response]
-
-
-if __name__ == '__main__':
-    httpd = make_server('', 8000, application)
-    print("Serving on port 8000...")
-    httpd.serve_forever()
-
+if __name__ == "__main__":
+    app.run()
